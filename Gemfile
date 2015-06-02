e7846a2e02febf64e29dcb9efb929181eea2ef64
Nyk Ma <moe@nayuki.info>
2015-06-02 15:40:26 +0800

    Finish getting list.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,8 @@ source 'http://ruby.taobao.org'
 
 gem 'nokogiri'
 gem 'httparty'
+gem 'activesupport'
+
+group :development do
+  gem 'byebug'
+end
--- main.rb
+++ main.rb
@@ -4,6 +4,9 @@ require 'bundler/setup'
 
 require "nokogiri"
 require "httparty"
+require 'active_support'
+require 'active_support/core_ext' # to_query
+require 'byebug'
 
 if ARGV.length == 0
   puts "At least one keyword should be given.\nExample: ./main.rb cat dog anime\nExit now."
@@ -13,13 +16,17 @@ end
 ARGV.each do |keyword|
   #puts keyword
   # Init
-  query ={ api_key: "dc6zaTOxFJmzC" , q: keyword.strip!} # Public API key, should be replaced in the future.
+  query ={ api_key: "dc6zaTOxFJmzC" , q: keyword.strip } # Public API key, should be replaced in the future.
   search_url = "http://api.giphy.com/v1/gifs/search?"
   # Get list.
+  byebug
   res = HTTParty.get(search_url + query.to_query)
   res = JSON.parse(res.body)["data"]
   res = res.map do |r|
     {url: r["images"]["original"]["url"], filename: r["id"] + ".gif"}
   end
+
+  puts res
+  exit
 
 end
